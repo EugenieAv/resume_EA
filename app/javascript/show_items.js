13f22ee0076624447b2d32a0e3805d4827d3bd45
Eugenie Avedikian <eugenieavedikian@gmail.com>
2021-01-13 10:15:01 +0100

    maj

--- app/javascript/show_items.js
+++ app/javascript/show_items.js
@@ -1,38 +1,73 @@
-const showItems = () => {
-  console.log("tutu");
+const makeMagicMail = () => {
+  anime.timeline()
+    .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 5400,
+    delay: (el, i) => 30 * i
+  }).add({
+    targets: '.ml16 .letters',
+    translateY: [-100,0],
+    duration: 5400,
+    easing: "easeOutExpo",
+    delay: (el, i) => 30 * i
+  });
+}
+// const makeMagicphone = () => {
+//   anime.timeline()
+//     .add({
+//     targets: '.ml16 .letters',
+//     translateY: [-100,0],
+//     easing: "easeOutExpo",
+//     duration: 5400,
+//     delay: (el, i) => 30 * i
+//   })
+//   // .add({
+//   //   targets: '.ml16',
+//   //   opacity: 0,
+//   //   duration: 1000,
+//   //   easing: "easeOutExpo",
+//   //   delay: 1000
+//   // })
+//   ;
+// }
 
-  const iconMail = document.querySelector('.fa-envelope');
-  const iconTel = document.querySelector('.fa-phone-alt');
-  const email = document.querySelector('#email');
+
+const showItems = () => {
+  const divMail = document.querySelector('#div-mail');
+  const divTel = document.querySelector('#div-tel');
+  const mail = document.querySelector('#email');
   const tel = document.querySelector('#tel');
 
-  const show = (obj) => {
-    obj.classList.remove('d-none');
-  }
+  mail.innerHTML = mail.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  tel.innerHTML = tel.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-  const hide = (obj) => {
-    obj.classList.add('d-none');
-  }
-
-  iconMail.addEventListener('mouseover', () =>
-    show(email)
-  );
-  iconMail.addEventListener('mouseout', () =>
-    hide(email)
-  );
-  iconTel.addEventListener('mouseover', () => {
+  divMail.addEventListener('mouseover', () =>{
+    mail.classList.remove('d-none');
+    anime.timeline()
+      .add({
+      targets: '.ml16 .letter',
+      translateY: [-100,0],
+      easing: "easeOutExpo",
+      duration: 3000,
+      delay: (el, i) => 30 * i
+    });
+  });
+  divTel.addEventListener('mouseover', () =>{
     tel.classList.remove('d-none');
-    tel.classList.add('visible');
-    tel.classList.remove('hidden');
+    anime.timeline()
+    .add({
+      targets: '.ml12 .letter',
+      translateX: [40,0],
+      translateZ: 0,
+      opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 2200,
+      delay: (el, i) => 500 + 30 * i
+    });
   });
-  iconTel.addEventListener('mouseout', () => {
-    tel.classList.add('hidden');
-    tel.classList.remove('visible');
-  });
-  // divTel.addEventListener('hover', console.log("iksooooo"));
-
 }
-
 
 export { showItems };
 
