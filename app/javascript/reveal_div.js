6f59465b37660da0b1f83db37d4c892df5ffd09b
Eugenie Avedikian <eugenieavedikian@gmail.com>
2021-01-18 18:04:17 +0100

    responsive part 1

--- app/javascript/reveal_div.js
+++ app/javascript/reveal_div.js
@@ -1,14 +1,22 @@
 const reavealDiv = () => {
-  console.log('abracadabraaaaa')
   const sections = document.querySelectorAll('.sections');
-  console.log(sections);
+  // const expandArrows = document.querySelectorAll('.fa-expand-alt');
 
   sections.forEach( section => {
     const divToReaveal = section.querySelector('.hide');
+    // const expandArrow = section.querySelector('.fa-expand-alt');
+    const arrow = section.querySelector('i');
 
-    section.addEventListener('click', () => {
+    arrow.addEventListener('click', () => {
      divToReaveal.classList.toggle('hide');
+     if (arrow.classList.contains('fa-expand-alt')){
+        arrow.classList.replace('fa-expand-alt', 'fa-compress-alt');
+     }
+      else {
+        arrow.classList.replace('fa-compress-alt', 'fa-expand-alt');
+     }
     });
+
   })
 
 };
