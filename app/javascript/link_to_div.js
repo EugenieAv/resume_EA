d62c101d772c058178eceb9530d45d32581da964
Eugenie Avedikian <eugenieavedikian@gmail.com>
2021-01-21 16:00:38 +0100

    on peut cliquer 2 fois dans le menu

--- app/javascript/link_to_div.js
+++ app/javascript/link_to_div.js
@@ -6,6 +6,7 @@ const linkToDiv = () => {
   const btnFive = document.getElementById("btnFive");
 
   const divs = document.querySelectorAll('.hide');
+  const markers = document.querySelectorAll('.marker');
 
   const target1 = document.getElementById('about-me');
   const target2 = document.getElementById('langage');
@@ -13,55 +14,64 @@ const linkToDiv = () => {
   const target4 = document.getElementById('experience');
   const target5 = document.getElementById('formation');
 
-  btnOne.addEventListener('click', () => {
-    event.preventDefault();
-    divs[0].classList.remove('hide');
-    target2.scrollIntoView({
+  const changeIcon = (target) => {
+    target.querySelector('i').classList.toggle('fa-expand-alt');
+    target.querySelector('i').classList.toggle('fa-compress-alt');
+  }
+
+  const scrollToTheDiv = (target) => {
+    target.scrollIntoView({
       block: 'start',
       behavior: 'smooth',
       inline: 'nearest'
-    });
+    })
+  }
+
+
+  const refacto = (item1, item2, item3) => {
+    if (item1.classList.contains('hide')){
+      item2.classList.remove('hide');
+      scrollToTheDiv(item3);
+      changeIcon(item3);
+    } else {
+      console.log('perdu')
+      item1.classList.toggle('hide');
+      changeIcon(item3);
+    }
+  }
+
+  btnOne.addEventListener('click', () => {
+    event.preventDefault();
+    refacto(markers[0], divs[0], target1);
+    // if (markers[0].classList.contains('hide')){
+    //   divs[0].classList.remove('hide');
+    //   scrollToTheDiv(target1);
+    //   changeIcon(target1);
+    // } else {
+    //   markers[0].classList.toggle('hide');
+    //   changeIcon(target1);
+    // }
   })
 
   btnTwo.addEventListener('click', () => {
     event.preventDefault();
-    divs[1].classList.remove('hide');
-    target2.scrollIntoView({
-      block: 'start',
-      behavior: 'smooth',
-      inline: 'nearest'
-    });
+    refacto(markers[1], divs[1], target2);
   })
 
   btnThree.addEventListener('click', () => {
     event.preventDefault();
-    divs[2].classList.remove('hide');
-    target3.scrollIntoView({
-      block: 'start',
-      behavior: 'smooth',
-      inline: 'nearest'
-    });
+    refacto(markers[2], divs[2], target3);
   })
 
   btnFour.addEventListener('click', () => {
     event.preventDefault();
-    divs[3].classList.remove('hide')
-    target4.scrollIntoView({
-      block: 'start',
-      behavior: 'smooth',
-      inline: 'nearest'
-    });
+    refacto(markers[3], divs[3], target4);
   })
 
 
   btnFive.addEventListener('click', () => {
     event.preventDefault();
-    divs[4].classList.remove('hide');
-    target5.scrollIntoView({
-      block: 'start',
-      behavior: 'smooth',
-      inline: 'nearest'
-    });
+    refacto(markers[4], divs[4], target5);
   })
 
 };
