e51b701eb0733ec0d0feba7c2d50874f7944475b
Eugenie Avedikian <eugenieavedikian@gmail.com>
2021-01-22 21:14:34 +0100

    schema de couleur defini

--- app/javascript/menu_fixed_on_top.js
+++ app/javascript/menu_fixed_on_top.js
@@ -6,10 +6,10 @@ const menuFixedOnTop = () => {
   // console.log(menu.offsetHeight);
   window.addEventListener('scroll', () => {
     if (window.scrollY >= menu.offsetTop){
-      menu.style.background = 'rgba(217, 217, 217, 0.9)';
-      menu.style.boxShadow = '3rem 3rem 10rem rgba(0, 0, 0, 0.4)';
+      menu.style.background = '#360055';
+      menu.style.boxShadow = '1rem 1rem 1rem rgba(0, 0, 0, 0.4)';
     } else {
-      menu.style.background = "rgba(255, 255, 255, 0.4)";
+      menu.style.background = "#360055";
       menu.style.boxShadow = "";
     }
   })
